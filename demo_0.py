0424fe87154eb156bc79683f1558df44f269ef65
bdrummo6 <bradley.drummonds@gmail.com>
2020-10-05 22:13:29 -0400

    Update demo_0.py

--- demo_0.py
+++ demo_0.py
@@ -6,6 +6,13 @@ print out the area of a circle to 3 decimal places in feet squared
 """
 
 # first pass attempt
-# how do we find the area of a circle?
+# how do we find the area of a circle? pi * radius** 2
 # lets just get the area and print it then work from there
 
+from math import pi
+
+radius = int(input('Enter the radius of a circle: '))
+
+area = pi * radius**2
+
+print(f'{area:.3f} ft\u00B2')
